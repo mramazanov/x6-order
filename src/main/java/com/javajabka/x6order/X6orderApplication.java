2cac2f2660072a22492c1140baed4938a8ac2398
Admin <mramazanov@outlook.com>
2025-03-05 16:29:27 +0300

    init

---
+++ src/main/java/com/javajabka/x6order/X6orderApplication.java
@@ -0,0 +1,11 @@
+package com.javajabka.x6order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6orderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(X6orderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javajabka/x6order/X6orderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.javajabka.x6order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6orderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
